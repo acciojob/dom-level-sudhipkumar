466687feae34cd9d04491d3a377e369a161e7741
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-04-02 22:16:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
         level++;
         element = element.parentElement;
     }
-
+//
     alert("The level of the element is: " + level);
 });
